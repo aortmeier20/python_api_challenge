c45ea1860d30fa3bc4c6ff7a864bf4ba0bd540b9
Austen <aortmeier20@gmail.com>
2024-04-24 18:29:40 -0500

    getting stuff ready

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "1f7275eceeb4bac8579dcba980abda02"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "dd6be2eb77e84c97a0af338cde98856e"
